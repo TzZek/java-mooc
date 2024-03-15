ef4bbe73b1c4f2062f8fa9147a2fb2c1adef58db
TzZek <tzzek@pm.me>
2024-03-14 22:29:21 -0700

    continue part 4 challenges

--- mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,11 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+        
+        Account matthewsAccount = new Account("Matthew's Account", 100.0);
+        matthewsAccount.deposit(20.0);
+        System.out.println(matthewsAccount);
+        
+        
     }
 }
---
+++ mooc-java-programming-i/part04-Part04_10.Song/src/main/java/Song.java
@@ -0,0 +1,18 @@
+
+public class Song {
+    private String name;
+    private int seconds;
+    
+    public Song(String name, int length){
+        this.name = name;
+        this.seconds = length;
+    }
+    
+    public String name(){
+        return this.name;
+    }
+    
+    public int length(){
+        return this.seconds;
+    }
+}
